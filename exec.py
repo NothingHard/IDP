41004dfebc285338274964c3ace3b5134a7821ed
cmchang <chunming00@icloud.com>
2017-10-19 11:10:52 +0800

    OY run

--- exec.py
+++ exec.py
@@ -1,7 +1,7 @@
 import os
 profile = ["all-one","harmonic"]
 alpha = [0.1,0.5,0.9]
-alternate = [1]
+alternate = [0,1] # gamma trainable
 act_type = ['relu']
 
 for p in profile:
@@ -9,4 +9,4 @@ for p in profile:
 		for alt in alternate:
 			for act in act_type:
 			#print("python IDP_stepwise_train.py {0} {1} {2}".format(p,a,alt))
-				os.system("python3 IDP_stepwise_train_stopbyvalloss_newATP.py {0} {1} {2} {3}".format(p,a,alt,act))
+				os.system("python3 IDP_stepwise_train_stopbyvalloss.py {0} {1} {2} {3}".format(p,a,alt,act))
